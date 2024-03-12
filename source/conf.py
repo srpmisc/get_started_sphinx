bc7bb947d7c72e990c4261a624174f8392f21170
srpmisc <sylliemisc@gmail.com>
2024-03-12 16:06:53 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -1,33 +1,95 @@
-# -*- coding: utf-8 -*-
-#
-# 
-source_suffix = '.rst'
+"""
+Imports the system and the 
+theme the project is using to the project.
+"""
+
+import sys, os
+import sphinx_bootstrap_theme
+
+"""
+Adds path to the folder ext, where extensions are stored.
+"""
+
+sys.path.append(os.path.abspath('ext'))
+sys.path.append('.')
+
+"""
+Tells Sphinx which extensions to use.
+"""
+
+extensions = ['xref', 
+              'youtube', 
+              'sphinx.ext.autosectionlabel',
+              'sphinxcontrib.osexample']
+
+"""
+Imports all link files into project.
+"""
+
+from links.link import *
+from links import *
+
+"""
+Tells the project where to look for theme templates and css overrides.
+"""
+templates_path = ['_templates']
+html_static_path = ["_static"]
+
+"""
+Tells the project the name of the home page.
+"""
 
 master_doc = 'index'
 
-# General information about the project.
-project = u'Sample Resume with Sphinx'
-copyright = u'2019, Mark Hoeber'
-author = u'Mark Hoeber'
+"""
+The project name, copyright, and author.
+"""
+
+project = u'RST | Sphinx | Sublime | GitHub'
+copyright = u'2024, fishiefishies'
+author = u'fishiefishies'
 
 
-#version = u'1.0'
-# The full version, including alpha/beta/rc tags.
-#release = u'1.0'
-
-
-language = None
-
+"""
+Tells the project to use sphinx pygments for color coding code examples.
+"""
 
 pygments_style = 'sphinx'
 
-
-#html_theme = 'default'
-html_theme = 'alabaster'
-
-rst_epilog = """
-.. include:: substitutions.txt
 """
+Tells the project to include content in todo directives.  Often set through 
+parameter to make commands.
+"""
+todo_include_todos = True
+
+"""
+Tells the project which theme to use, and the theme options.
+"""
+html_theme = 'default'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+
+html_theme_options = {
+
+  'navbar_site_name': "Contents",
+  'source_link_position': "footer",
+}
+
+def setup(app):
+    app.add_stylesheet("my-styles.css") 
+    
+"""
+Tells the project to ignore certain files in the build process.
+"""
+exclude_patterns = ['substitutions.rst']
+
+"""
+Tells the project to append content at the end of every file during the build 
+process.
+"""
+rst_epilog = """
+.. include:: substitutions.rst
+"""
+
 
 
 
